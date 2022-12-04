f282608c301a0a9304cc5e3a38a35356ece6ad2d
ivan_cclin <ivan_cclin@tutorabc.com>
2022-12-04 22:29:32 +0800

    linelifflogin

---
+++ MartechLiff/Controllers/LineController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace MartechLiff.Controllers
+{
+    public class LineController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public LineController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLiff()
+        {
+            return View();
+        }
+
+    }
+}
